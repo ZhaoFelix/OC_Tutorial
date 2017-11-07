90748903f9044c640493df36ef56c2428cc9e7c6
Felix <felix@angellecho.com>
2017-11-07 18:30:52 +0800

    no message

---
+++ Git/Git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git
+//
+//  Created by Felix on 2017/11/7.
+//  Copyright © 2017年 Felix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
